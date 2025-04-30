5fdf0656c259591e57e18f37acd7fba0dabaca8f
HBDCHAEYOUNG <devyoon02@gmail.com>
2025-04-30 23:12:02 +0900

    madal page update

---
+++ src/app/(before login)/@modal/(.)i/flow/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginModal() {
+  return (
+    <div className="bg-transparent absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 border-2 border-red-500 z-50">
+      Login Modal
+    </div>
+  );
+}
