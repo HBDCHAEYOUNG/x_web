ca45316229ed72a84ddc6cc90974ca57f76e9287
HBDCHAEYOUNG <devyoon02@gmail.com>
2025-05-01 23:12:02 +0900

    madal page ui update

--- src/app/(before login)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(before login)/@modal/(.)i/flow/login/page.tsx
@@ -1,7 +1,12 @@
 export default function LoginModal() {
   return (
-    <div className="bg-transparent absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 border-2 border-red-500 z-50">
-      Login Modal
+    <div
+      className="fixed inset-0 flex items-center justify-center z-50"
+      style={{ backgroundColor: "rgba(107, 114, 128, 0.3)" }}
+    >
+      <div className="bg-white w-1/2 h-1/2 rounded-lg shadow-lg">
+        Login Modal
+      </div>
     </div>
   );
 }
