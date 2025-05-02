8090da59a54aec936953f9c95b5e7d52de9efb9b
HBDCHAEYOUNG <devyoon02@gmail.com>
2025-05-02 23:12:02 +0900

    modal ui update

--- src/app/(before login)/i/flow/login/page.tsx
+++ src/app/(before login)/i/flow/login/page.tsx
@@ -1,8 +1,28 @@
+import Link from "next/link";
+
 export default function LoginModal() {
   return (
     <div className="fixed inset-0 bg-gray-50 bg-opacity-10 flex items-center justify-center z-50">
-      <div className="bg-white w-1/2 h-1/2 rounded-lg shadow-lg">
-        Login Modal
+      <div className="bg-white w-1/2 h-1/2 rounded-lg shadow-lg flex flex-col justify-center">
+        <div className="flex flex-col gap-4 mx-auto w-1/3">
+          <h1 className="text-2xl font-bold">X가입하기</h1>
+          <input
+            type="text"
+            placeholder="이메일"
+            className="rounded-full border border-brand-grey p-2 w-full"
+          />
+          <input
+            type="text"
+            placeholder="비밀번호"
+            className="rounded-full border border-brand-grey p-2 w-full"
+          />
+          <button className="bg-black text-white px-4 p-2 rounded-full w-full">
+            다음
+          </button>
+          <p className="text-sm text-gray-500">
+            이미 계정이 있으신가요? <Link href="/login">가입하기</Link>
+          </p>
+        </div>
       </div>
     </div>
   );
